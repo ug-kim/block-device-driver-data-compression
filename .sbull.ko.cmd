074481964a491364e2e365c5c2f20d7befd691fa
ug-kim <ugkim816@gmail.com>
2020-12-18 16:32:36 +0000

    [#8] complete table

--- .sbull.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/dgist/project/sbull.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/dgist/project/sbull.ko /home/dgist/project/sbull.o /home/dgist/project/sbull.mod.o ;  true
--- sbull.c
+++ sbull.c
@@ -56,7 +56,8 @@ int key; // 0 is true, 1 is false
  */
 #define INVALIDATE_DELAY	30*HZ
 
-int table[n_sects][2] = {{0}};
+// index 0 : com_ret, 1 : src_len, 2 : is_compress
+int table[n_sects][3] = {{0}};
 		
 /*
  * The internal representation of our device.
@@ -106,16 +107,19 @@ static void sbull_transfer(struct sbull_dev *dev, unsigned long sector,
 	
 
 	if(write) {
+		// if(sector < 500) {
 		printk("write -------------------------\n");
 		printk("source: %s\n", buffer);
+		// }
 		src_len = strlen(buffer);
 		
 		if (src_len == 0) {
 			memcpy(dev->data + offset, buffer, nbytes);
 		} else {
 			// compression
+			// if (sector < 500) {
 			printk(KERN_INFO "[compress] Compress mode turn on !!!\n");
-			
+			// }
 			unsigned char *buf;
 			unsigned char *dst;
 			
@@ -128,14 +132,18 @@ static void sbull_transfer(struct sbull_dev *dev, unsigned long sector,
 				pr_err("LZ4 compress_default error: compression failed!\n");
 			}
 
-			memcpy(dev->data + offset, dst, nbytes);
-			
-			table[sector][0] = com_ret;
-			table[sector][1] = src_len;
-
+			if (com_ret > src_len) {
+				memcpy(dev->data + offset, buffer, nbytes);
+				table[sector][2] = 1;
+			} else {
+				memcpy(dev->data + offset, dst, nbytes);
+				table[sector][0] = com_ret;
+				table[sector][1] = src_len;
+			}
+			// if(sector < 50000) {
 			printk("[compress] source: %s, len: %d\n", buffer, strlen(buffer));
 			printk("[compress] encode: %s, len: %d\n", dst, com_ret);
-
+			// }
 			// printk("source len at compress: %d\n", src_len);
 			// printk("com_ret at compress: %d\n", com_ret);
 			// printk("================================\n");
@@ -146,14 +154,19 @@ static void sbull_transfer(struct sbull_dev *dev, unsigned long sector,
 //	else memcpy(buffer, dev->data + offset, nbytes);
 	else {
 		
+		// if (sector < 50000) {
 		printk("read -------------------------\n");
-		printk("dst: %s\n", buffer);
-		printk("dst2: %s\n", buffer);
+		// }
 		// printk("nbytes at decompress: %d\n", nbytes);	
 		com_ret = table[sector][0];
 		src_len = table[sector][1];
 		
-		if (src_len == 0) {
+		int is_compress = table[sector][2];
+		
+		// if (sector < 50000) {
+		printk("com_ret: %d, src_len: %d\n", com_ret, src_len);
+		// }
+		if (src_len == 0 || is_compress == 1) {
 			memcpy(buffer, dev->data + offset, nbytes);
 		} else {
 			// decompression
@@ -163,8 +176,10 @@ static void sbull_transfer(struct sbull_dev *dev, unsigned long sector,
 			memcpy(dst, dev->data + offset, com_ret);
 			// printk("source len at decompress: %d\n", src_len);
 			// printk("com_ret at decompress: %d\n", com_ret);
+			
+			// if (sector < 50000) {
 			printk("[decompress] dst: %s, len: %d\n", dst, com_ret);
-				
+			// }
 			unsigned char *dec;
 			int dcom_ret;
 			dec = kmalloc(src_len, GFP_KERNEL);
@@ -173,7 +188,9 @@ static void sbull_transfer(struct sbull_dev *dev, unsigned long sector,
 				memcpy(buffer, dev->data + offset, nbytes);
 				pr_err("LZ4_decompress_safe error, ret = %d\n", dcom_ret);
 			} else {
+				// if (sector < 50000) {
 				printk("[decompress] dec: %s, len: %d\n", dec, src_len);
+				// }
 				memcpy(buffer, dec, nbytes);
 			}
 
@@ -182,11 +199,15 @@ static void sbull_transfer(struct sbull_dev *dev, unsigned long sector,
 			kfree(dst);
 			kfree(dec);
 		}
+		// if (sector < 50000) {
 		printk("dst3: %s\n", buffer);
+		// }
 	}
-	
-	printk("sector: %ld, nsect: %ld\n", sector, nsect);
-	printk("offset: %ld, nbytes: %ld\n", offset, nbytes);
+
+	// if (sector < 50000) {
+		printk("sector: %ld, nsect: %ld\n", sector, nsect);
+		printk("offset: %ld, nbytes: %ld\n", offset, nbytes);
+	// }
 }
 
 
